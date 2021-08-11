0f543f9e8f3cc03a84aaa8a5b8a1f0b225d41272
Angel <angeloxv@outlook.it>
2021-08-11 23:46:50 +0200

    part9: 9.3

--- bmi/bmiCalculator.ts.ts
+++ bmi/bmiCalculator.ts.ts
@@ -1,15 +1,19 @@
-const calculateBmi = (height: string, weight: string): String => {
-  const bmi = Number(weight) / (Number(height) / 100) ** 2
-  // console.log(bmi)
-  if (bmi < 16) return 'Underweight (Severe thinness)'
-  if (bmi < 16.9) return 'Underweight (Moderate thinness)'
-  if (bmi < 18.4) return 'Underweight (Mild thinness)'
-  if (bmi < 24.9) return 'Normal range'
-  if (bmi < 29.9) return 'Overweight (Pre-obese)'
-  if (bmi < 34.9) return 'Obese (Class II)'
-  if (bmi < 39.9) return 'Obese (Class II)'
-  return 'Obese (Class III)'
-}
+const calculateBmi = (height: number, weight: number): String => {
+  const bmi = weight / (height / 100) ** 2;
 
-const [height, weight] = process.argv.slice(2, 4)
-console.log(calculateBmi(height, weight))
+  if (bmi < 16) return 'Underweight (Severe thinness)';
+  if (bmi < 16.9) return 'Underweight (Moderate thinness)';
+  if (bmi < 18.4) return 'Underweight (Mild thinness)';
+  if (bmi < 24.9) return 'Normal range';
+  if (bmi < 29.9) return 'Overweight (Pre-obese)';
+  if (bmi < 34.9) return 'Obese (Class II)';
+  if (bmi < 39.9) return 'Obese (Class II)';
+  return 'Obese (Class III)';
+};
+
+let [inHeight, inWeight] = process.argv.slice(2, 4);
+const height = Number(inHeight),
+  weight = Number(inWeight);
+if (!(isNaN(height) || isNaN(weight))) {
+  console.log(calculateBmi(height, weight));
+}
