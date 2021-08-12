740f8d79804e4e33f7edb344407e9137b63b83e5
Angel <angeloxv@outlook.it>
2021-08-12 19:02:40 +0200

    part9: 9.9

--- patientor/backend/build/index.js
+++ patientor/backend/build/index.js
@@ -4,10 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
-const PORT = 4000;
-app.use(express_1.default.json());
-app.get('/ping', (_req, res) => {
-    res.send('pong');
+const PORT = 3001;
+app.use(express_1.default.json(), cors_1.default());
+app.get('/api/ping', (_req, res) => {
+    return res.send('pong');
+});
+app.get('*', (_req, res) => {
+    console.error('There route does not exist');
+    return res.status(404).send('There route does not exist');
 });
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-const PORT = 4000;
+const PORT = 3001;
 
-app.use(express.json());
+app.use(express.json(), cors());
 
-app.get('/ping', (_req, res) => {
-  res.send('pong');
+app.get('/api/ping', (_req, res) => {
+  return res.send('pong');
+});
+app.get('*', (_req, res) => {
+  console.error('There route does not exist');
+  return res.status(404).send('There route does not exist');
 });
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
