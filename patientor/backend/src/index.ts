723d837ed22ff8b450bad66213d573debcbaec86
Angel <angeloxv@outlook.it>
2021-08-13 08:54:11 +0200

    part9: 9.10 and code refactor

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 
+import diagnoseRouter from './routes/diagnoses';
+
 const app = express();
 const PORT = 3001;
 
 app.use(express.json(), cors());
 
+app.use('/api/diagnoses', diagnoseRouter);
 app.get('/api/ping', (_req, res) => {
   return res.send('pong');
 });
+
 app.get('*', (_req, res) => {
   console.error('There route does not exist');
   return res.status(404).send('There route does not exist');
